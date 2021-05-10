4dad309e4768354d585cf6aff7cbe0770e76761a
utkarshgubrelay <utkarshgubrelay2000@gmail.com>
2021-05-10 18:39:06 +0530

    controller

---
+++ controller/registerController.js
@@ -0,0 +1,33 @@
+
+exports.homePage = (req, res) => {
+    res.render('index')
+}
+
+
+exports.contact = (req, res) => {
+    res.render('contact')
+}
+exports.about = (req, res) => {
+    res.render('about')
+}
+exports.work = (req, res) => {
+    res.render('work')
+}
+exports.Lab = (req, res) => {
+    res.render('Lab')
+}
+exports.service = (req, res) => {
+    res.render('services')
+}
+exports.careers = (req, res) => {
+    res.render('careers')
+}
+exports.team = (req, res) => {
+    res.render('team')
+}
+exports.why_us = (req, res) => {
+    res.render('why_us')
+}
+exports.privacy = (req, res) => {
+    res.render('privacy')
+}
